1d788ff8b25570e2d8f6cbdfb230b85c97f590ee
Mario Cruz <mario@choosedigital.com>
2016-03-24 23:49:29 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import printer, textwrap, sys 
 
 p=printer.ThermalPrinter(serialport="/dev/ttyAMA0") 
-unwrapped_text = "\nHello NightShift & RSM Good Morning from My Pi, Roses are red, violets are blue, I'm seeing a future filled with Poem and you!\n"
+unwrapped_text = "\n From My Pi, Roses are red, violets are blue, I'm seeing a future filled with Poem and you!\n"
 wrapped_text = textwrap.fill(unwrapped_text, 32) 
 p.print_text(wrapped_text)
 p.linefeed() 
