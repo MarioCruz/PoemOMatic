e86736dde9f277ebdfd1d610924c02d9b3bae695
Mario Cruz <mario@choosedigital.com>
2016-04-03 09:38:28 -0400

    Update test.py
    Added Pi 3 support

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 
 import printer, textwrap, sys 
 
-p=printer.ThermalPrinter(serialport="/dev/ttyAMA0") 
+
+ # Pi 1 and 2 
+   p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+ 
+  # Pi 3
+  #  p = printer.ThermalPrinter(serialport="/dev/ttyS0")
+
 unwrapped_text = "\n From My Pi, Roses are red, violets are blue, I'm seeing a future filled with Poems and you!\n"
 wrapped_text = textwrap.fill(unwrapped_text, 32) 
 p.print_text(wrapped_text)
